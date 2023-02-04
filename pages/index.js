1cc9556ca047fbb50855be4731176e484d4af6f0
Pradeep Keshary <puntubabu@gmail.com>
2023-02-04 02:41:41 -0800

    complete linking, styling, dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from "../lib/posts";
+import Link from 'next/link';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostData = getSortedPostsData();
+  return {
+    props: {
+      allPostData,
+    }
+  }
+}
+export default function Home({ allPostData }) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +21,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>sha na na na knees knees</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
